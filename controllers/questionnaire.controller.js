e615b29ab48dbdb91448a30e8b3dc770622bc5d2
praveen <praveen.mk4@hotmail.com>
2019-11-29 11:07:30 +0530

    Added to get random Questions based on tech and number

--- controllers/questionnaire.controller.js
+++ controllers/questionnaire.controller.js
@@ -1,33 +1,34 @@
 const Questionnaire = require('../models/questionnaire.model').Questionnaire;
+const mongoose = require('mongoose');
 module.exports.addQuestions = (req, res) => {
     console.log('in creaedasfadsfasdfasdfasdfasddf');
     const Question = {
-        "technology":req.body.technology,
-        "questions":req.body.questions
+        "technology": req.body.technology,
+        "questions": req.body.questions
     };
-    let question = { technology: Question.technology,questions:Question.questions};
+    let question = { technology: Question.technology, questions: Question.questions };
 
     var questionObj = {};
     console.log(Question.questions);
- 
+
     Questionnaire.findOne({ technology: Question.technology }).exec((err, Data) => {
         if (err) {
             res.send(err);
         } else if (Data !== null) {
-            for (var i=0;i< Question.questions.length;i++){
+            for (var i = 0; i < Question.questions.length; i++) {
                 questionObj.question = Question.questions[i].question;
                 questionObj.questionType = Question.questions[i].questionType;
                 questionObj.options = Question.questions[i].options;
             }
-          Questionnaire.findOneAndUpdate({technology: Question.technology}, { $push: { questions: questionObj } }).exec((err,record) =>{
-            if (err) {
-                res.send(err);
-            } else {
-              res.send(record);
-            }
-          });
+            Questionnaire.findOneAndUpdate({ technology: Question.technology }, { $push: { questions: questionObj } }).exec((err, record) => {
+                if (err) {
+                    res.send(err);
+                } else {
+                    res.send(record);
+                }
+            });
         } else {
-           
+
             let newQuestion = new Questionnaire(question);
             newQuestion.save((error, result) => {
                 if (error) {
@@ -36,6 +37,15 @@ module.exports.addQuestions = (req, res) => {
                     res.send(result);
                 }
             });
+        }
+    });
+};
+module.exports.getRandomQuestion = (req, res) => {
+      console.log(req.params);
+      const number = Number(req.params.number);
+    Questionnaire.aggregate([{$match: { technology: req.params.technology}} ,{ "$unwind": "$questions" }  , { "$sample": { "size":number}}  ]).exec((err, Data) => {
+        if (err) { res.send(err) } else {
+            res.send(Data);
         }
     });
 };
--- routes/questionnaireRoute.js
+++ routes/questionnaireRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Questions = require('../controllers/questionnaire.controller');
-
+router.get('/getRandomQuestions/:technology/:number',Questions.getRandomQuestion);
 router.post('/addQuestions',Questions.addQuestions);
+
 module.exports = router;
